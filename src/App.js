d93a2a13d2958f4671e37eeff24b2d29d740e8f6
shravanjaju <shravanjaju77@gmail.com>
2023-04-27 01:16:10 +0530

    NewNameCard

--- src/App.js
+++ src/App.js
@@ -4,34 +4,6 @@ import {useState} from 'react'
 
 export default function App() {
 
-const [cnt,setcnt]=useState(0)
-
-function increasenum(){
-  const counts=cnt+1
-  setcnt(counts)
-}
-function decreasenum(){
-  const countss=cnt-1
-  setcnt(countss)
-}
-
-
-
-
-
-return(
-  <div className='newdiv'>
-    <h3> Count is:  {cnt}</h3>
-    <button onClick={increasenum}>Increment</button> 
-    <button onClick={decreasenum}>Decrement</button>
-  </div>
-
-)
-
-
-
-
-
 
 }
 
