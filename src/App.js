6ee05a9e088c55d0d9c285131aa9aba1c998c8bf
shravanjaju <shravanjaju77@gmail.com>
2023-05-04 02:07:00 +0530

    genOTP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Fetchdata from './Components/useFetchs';
+import React from "react";
 
-function App() {
-  return (
-   <Fetchdata/>
-  );
+export default function App(){
+function sendOTP(mobileNumber) {
+  const apiUrl = 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP';
+  const OTP = generateOTP();
+  const data = { mobileNumber: mobileNumber, OTP: OTP };
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('OTP sent to', mobileNumber);
+    } else {
+      console.log('OTP not sent');
+    }
+  })
+  .catch(error => {
+    console.error('Error sending OTP:', error);
+  });
 }
-
-export default App;
+return(
+  <div>
+    <button onClick={generateOTP}>Generate Otp</button>
+  </div>
+)}
