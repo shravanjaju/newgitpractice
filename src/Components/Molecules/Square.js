cdb5b9195b4531a1828b6f5abc97bfa43fd62c7c
shravanjaju <shravanjaju77@gmail.com>
2023-05-12 03:20:08 +0530

    full game

--- src/Components/Molecules/Square.js
+++ src/Components/Molecules/Square.js
@@ -3,11 +3,11 @@ import React from "react";
 const SquareC = (props) => {
     const classes = (props.className ? `${props.className} square` : `square`)
     return (
-        <span
+        <div
             className={classes + (props.state === "X" ? ` fc-aqua` : ` fc-white`)}
             onClick={() => props.onClick(props.index)}>
            {props.state}
-        </span>
+        </div>
     )
 }
 export default SquareC
