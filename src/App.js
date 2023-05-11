bd18526090564f1818b3d7dc606549372d21c7cc
shravanjaju <shravanjaju77@gmail.com>
2023-05-12 03:11:41 +0530

    full game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import Squarec from './Components/Molecules/Square';
-
-export default function App() {
-  return (
-    <div className="Appheader">
-     <p className='heading'> Tic-Tac-Toe By ShravanJ </p>
-     <div className='row center'>
-      <Squarec className="b-bottom-right"/>
-      <Squarec className="b-bottom-right"/>
-      <Squarec className="b-bottom-right"/> </div>
-
-     <div className='row center'>
-     <Squarec className="b-bottom-right"/>
-     <Squarec className="b-bottom-right"/>
-     <Squarec className="b-bottom-right"/> </div>
-     <div className='row center'>
-      <Squarec className="b-bottom-right"/>
-     <Squarec className="b-bottom-right"/>
-     <Squarec className="b-bottom-right"/> </div>
-     <button className='learbutton'>Clear</button>
-    </div>
-  );
+import Main from './Components/Molecules/Main';
+function App() {
+    return(
+      <Main/>
+    );
 }
 
-
+export default App;
